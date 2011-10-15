6443509fa9b1811811d8982bef764ff2b280cc13
hatone <b1006006@gmail.com>
2011-10-16 04:37:03 +0900

    change config-dist.php

--- public_html/config-dist.php
+++ public_html/config-dist.php
@@ -38,12 +38,12 @@ $CFG = new stdClass();
 // will be stored.  This database must already have been created         //
 // and a username/password created to access it.                         //
 
-$CFG->dbtype    = 'pgsql';      // 'pgsql', 'mysqli', 'mssql' or 'oci'
+$CFG->dbtype    = 'mysql';      // 'pgsql', 'mysqli', 'mssql' or 'oci'
 $CFG->dblibrary = 'native';     // 'native' only at the moment
-$CFG->dbhost    = 'localhost';  // eg 'localhost' or 'db.isp.com' or IP
-$CFG->dbname    = 'moodle';     // database name, eg moodle
-$CFG->dbuser    = 'username';   // your database username
-$CFG->dbpass    = 'password';   // your database password
+$CFG->dbhost    = 'DUMMY_DB_HOST';  // eg 'localhost' or 'db.isp.com' or IP
+$CFG->dbname    = 'DUMMY_PROJECT_NAME';     // database name, eg moodle
+$CFG->dbuser    = 'DUMMY_PROJECT_NAME';   // your database username
+$CFG->dbpass    = 'DUMMY_DB_PASSWORD';   // your database password
 $CFG->prefix    = 'mdl_';       // prefix to use for all table names
 $CFG->dboptions = array(
     'dbpersist' => false,       // should persistent database connections be
